08ff0c80ee2bc3ea7da0339784aced70c321393c
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-08-29 01:51:49 +0500

    Update swQuizApp.js

--- public/swQuizApp.js
+++ public/swQuizApp.js
@@ -7,6 +7,8 @@ var urlsToCache = [
     '/static/js/1.chunk.js',
     '/static/js/bundle.js',
     '/static/js/main.chunk.js', 
+    '/static/js/2.2f87d904.chunk.js',
+    '/static/js/main.4859b9f0.chunk.js'
     '/manifest.json',
     '/logo192.png',
     "index.html",    
