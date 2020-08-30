93b3a1f272897a538b88c77f3d45e5424b57c98b
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-08-30 05:38:27 +0500

    all error solved

--- public/swQuizApp.js
+++ public/swQuizApp.js
@@ -1,4 +1,5 @@
 
+
 var CACHE_NAME = 'quiz-app' 
 
 var urlsToCache = [
@@ -7,11 +8,6 @@ var urlsToCache = [
  '/static/js/1.chunk.js',
  '/static/css/2.11829350.chunk.css',
  '/static/js/main.chunk.js',
- '/main.fdd55bff2343a3656490.hot-update.js',    
- '/static/css/main.8ee7fde6.chunk.css',
- '/static/js/2.733aefd4.chunk.js',
- '/static/js/main.eb83e8a2.chunk.js',
- '/static/js/main.034afd45.chunk.js',
  '/manifest.json',
  '/logo192.png',
  "index.html",        
@@ -24,62 +20,62 @@ var urlsToCache = [
 
 this.addEventListener('install', (event) => {
 
- event.waitUntil(
-     caches.open(CACHE_NAME)
-         .then((cache) => {
-             console.log('Opened Cache')
-             return (
-                 cache.addAll(urlsToCache)
-                 
-             )
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened Cache')
+                return (
+                    cache.addAll(urlsToCache)
+                    
+                )
 
-         })
- )
+            })
+    )
 
 })
 
 
 
 const options = {
- ignoreSearch: true,
- ignoreMethod: true,
- ignoreVary: true
-};
+    ignoreSearch: true,
+    ignoreMethod: true,
+    ignoreVary: true
+  };
 
 
 this.addEventListener('fetch', (event) => {
- if (!navigator.onLine) {
-     event.respondWith(
-         caches.match(event.request, options)
-             .then((response) => {
-                 if (response) {
-                     console.log(response)
-                     return (response)
-                 } else {
-                     return fetch(event.request).then((response) => {
-                         if (!response || response.status !== 200 || response.type !== 'basic') {
-                             console.log(response)
-                             return response;
-                         }
-                         var responseToCache = response.clone();
+    if (!navigator.onLine) {
+        event.respondWith(
+            caches.match(event.request, options)
+                .then((response) => {
+                    if (response) {
+                        console.log(response)
+                        return (response)
+                    } else {
+                        return fetch(event.request).then((response) => {
+                            if (!response || response.status !== 200 || response.type !== 'basic') {
+                                console.log(response)
+                                return response;
+                            }
+                            var responseToCache = response.clone();
 
-                         caches.open(CACHE_NAME).then((cache) => {
-                             cache.put(event.request, responseToCache)
-                         })
-                         console.log(response)
-                         return response;
+                            caches.open(CACHE_NAME).then((cache) => {
+                                cache.put(event.request, responseToCache)
+                            })
+                            console.log(response)
+                            return response;
 
-                     }).catch((err) => {
-                         console.log('err', err)
-                     })
+                        }).catch((err) => {
+                            console.log('err', err)
+                        })
 
-                 }
+                    }
 
-             }).catch((err) => {
-                 console.log('err', err)
+                }).catch((err) => {
+                    console.log('err', err)
 
-             })
-     )
- }
+                })
+        )
+    }
 
 })
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
-import swQuizAppDev from './swQuizApp'
+import swQuizAppDev from './swQuizApp';
 
 ReactDOM.render(
   <React.StrictMode>
