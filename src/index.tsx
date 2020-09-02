a70f4221d8ef08f520202f0ff1cc3784b23450dd
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-09-03 03:50:45 +0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// if ("serviceWorker" in navigator) {
-//   navigator.serviceWorker
-//     .register("./firebase-messaging-sw.js")
-//     .then(function(registration) {
-//       console.log("Registration successful, scope is:", registration.scope);
-//     })
-//     .catch(function(err) {
-//       console.log("Service worker registration failed, error:", err);
-//     });
-// }
-
 
 ReactDOM.render(
   <React.StrictMode>
