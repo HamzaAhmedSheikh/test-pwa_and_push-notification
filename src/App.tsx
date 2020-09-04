1821c430c22e06024b075f9b2ee0cfd96199f659
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-09-04 05:31:30 +0500

    add .travis.yml

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import './App.css';
 
+
+
 import { initNotification } from './FCM'
+
 
 
 function App() {
 
+  
+
     return (
     <div className="App">
+                      
                                 
       Hello World <br />  <br /> <br />
       Progressive Web App and Push Notifications. <br />
       Firebase Cloud Messaging. <br />
        
-      My name is Hamza.  <br /> <br /> <br />     
+      My name is Hamza.  <br /> <br /> <br />
 
+          
+     
       <button onClick={initNotification}> suscribe to notification  </button> 
     </div>
   );
